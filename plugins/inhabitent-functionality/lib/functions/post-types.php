e982952afcf334d1d761881348d35cd7733dfbd2
Rose Cass <rose.cass12@gmail.com>
2018-02-20 13:30:13 -0800

    Added functionality plugin and product template pages.

---
+++ plugins/inhabitent-functionality/lib/functions/post-types.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * POST TYPES
+ *
+ * @link  http://codex.wordpress.org/Function_Reference/register_post_type
+ */
+
+// Register Product Custom Post Type
+function product_post_type() {
+
+	$labels = array(
+		'name'                  => 'Products',
+		'singular_name'         => 'Product',
+		'menu_name'             => 'Products',
+		'name_admin_bar'        => 'Products',
+		'archives'              => 'Product Archives',
+		'attributes'            => 'Product Attributes',
+		'parent_item_colon'     => 'Parent Product:',
+		'all_items'             => 'All Products',
+		'add_new_item'          => 'Add New Product',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New Product',
+		'edit_item'             => 'Edit Product',
+		'update_item'           => 'Update Product',
+		'view_item'             => 'View Product',
+		'view_items'            => 'View Products',
+		'search_items'          => 'Search Product',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into Product',
+		'uploaded_to_this_item' => 'Uploaded to this Product',
+		'items_list'            => 'Product list',
+		'items_list_navigation' => 'Product list navigation',
+		'filter_items_list'     => 'Filter Product list',
+	);
+	$args = array(
+		'label'                 => 'Product',
+		'description'           => 'Store Items',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-universal-access-alt',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'product', $args );
+
+}
+add_action( 'init', 'product_post_type', 0 );
---
+++ themes/inhabitent/archive-product.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying product archive page.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+  <h1> This is the shop page </h1>
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					get_template_part( 'template-parts/content' );
+				?>
+
+			<?php endwhile; ?>
+
+			<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
